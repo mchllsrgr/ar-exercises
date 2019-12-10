bd75ec054b819b953dbe184c4abfc4ddabbe078c
Michelle Siregar <michellesiregar@gmail.com>
2019-12-10 18:16:16 +0000

    create store-employee association, create new employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,9 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Chris", last_name: "Christopher", hourly_rate: 60)
+@store2.employees.create(first_name: "John", last_name: "Johns", hourly_rate: 10)
+@store1.employees.create(first_name: "Vero", last_name: "Veronicas", hourly_rate: 55)
+@store2.employees.create(first_name: "Bale", last_name: "Baileys", hourly_rate: 30)
+@store1.employees.create(first_name: "Pop", last_name: "Poppets", hourly_rate: 20)
+@store2.employees.create(first_name: "Alex", last_name: "Alexton", hourly_rate: 22)
